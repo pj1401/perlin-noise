c586164623decf6669106aec04b3ec1f7b1d9a13
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-01 19:58:55 +0200

    Add the Point class

---
+++ src/Point.js
@@ -0,0 +1,24 @@
+/**
+ * @file The Point class.
+ * @author Patricia Johansson <pj222uc@student.lnu.se>
+ * @version 1.0.0
+ */
+
+/**
+ * Represents a point on a grid.
+ */
+export class Point {
+  #x
+  #y
+
+  /**
+   * Initialises the object.
+   *
+   * @param {number} x - The x-coordinate.
+   * @param {number} y - The y-coordinate.
+   */
+  constructor (x, y) {
+    this.#x = x
+    this.#y = y
+  }
+}
