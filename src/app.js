d53702bed616bb725948c4829b991bf114c25f54
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-02 16:29:39 +0200

    Add test case for Point class

--- src/app.js
+++ src/app.js
@@ -5,11 +5,16 @@
  */
 
 import { Hello } from './Hello.js'
+import { Point } from './Point.js'
 
 try {
   const hello = new Hello()
 
   console.log(hello.toString())
+
+  const point0 = new Point(1, 2)
+
+  console.log(`(${point0.x}, ${point0.y})`)
 } catch (error) {
   console.error(error.message)
 }
