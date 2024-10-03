b87dbbead2ec7fae05d66569859f7cd79f2c8732
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-03 11:45:44 +0200

    Update tests for perlin noise values

--- test/PerlinNoise.test.js
+++ test/PerlinNoise.test.js
@@ -10,28 +10,28 @@ import { PerlinNoise } from '../src/PerlinNoise.js'
 test('Perlin noise values', () => {
   const perlin0 = new PerlinNoise(1.3, 2.1)
 
-  expect(perlin0.valueOf()).toBeCloseTo(0.239)
+  expect(perlin0.valueOf()).toBeCloseTo(0.093744)
 })
 
 test('Perlin noise values', () => {
   const perlin1 = new PerlinNoise(5.5, 3.7)
 
-  expect(perlin1.valueOf()).toBeCloseTo(-0.158)
+  expect(perlin1.valueOf()).toBeCloseTo(-0.177968)
 })
 
 test('Perlin noise values', () => {
   const perlin2 = new PerlinNoise(10.0, 20.0)
 
-  expect(perlin2.valueOf()).toBeCloseTo(0.107)
+  expect(perlin2.valueOf()).toBeCloseTo(0)
 })
 
 test('Perlin noise values', () => {
   const perlin3 = new PerlinNoise(100.0, 200.0)
 
-  expect(perlin3.valueOf()).toBeCloseTo(0.294)
+  expect(perlin3.valueOf()).toBeCloseTo(0)
 })
 
 test('Perlin noise values', () => {
   const perlin4 = new PerlinNoise(0.5, 0.5)
-  expect(perlin4.valueOf()).toBeCloseTo(0.467)
+  expect(perlin4.valueOf()).toBeCloseTo(0.006776)
 })
