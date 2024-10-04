8c29d634d76d19ea3ee7c1ba7de674ee8d40c7f3
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-04 12:25:23 +0200

    Add more test points for RandomGradients

--- test/RandomGradient.test.js
+++ test/RandomGradient.test.js
@@ -10,10 +10,16 @@ import { RandomGradient } from '../src/RandomGradient.js'
 
 test('Generate random gradient', () => {
   const randomGradients = [
+    { gradient: new RandomGradient(new Point(0, 0)), expected: { x: 1, y: 0 } },
     { gradient: new RandomGradient(new Point(1, 2)), expected: { x: 0.101279206, y: 0.994858041 } },
     { gradient: new RandomGradient(new Point(2, 2)), expected: { x: 0.007886916, y: -0.999968898 } },
     { gradient: new RandomGradient(new Point(1, 3)), expected: { x: 0.937233865, y: -0.348701422 } },
-    { gradient: new RandomGradient(new Point(2, 3)), expected: { x: -0.037835073, y: 0.999283997 } }
+    { gradient: new RandomGradient(new Point(2, 3)), expected: { x: -0.037835073, y: 0.999283997 } },
+    { gradient: new RandomGradient(new Point(6, 4)), expected: { x: -0.722342956, y: 0.691534998 } },
+    { gradient: new RandomGradient(new Point(10, 20)), expected: { x: -0.992802137, y: 0.119766091 } },
+    { gradient: new RandomGradient(new Point(11, 21)), expected: { x: -0.821464307, y: 0.570259934 } },
+    { gradient: new RandomGradient(new Point(100, 200)), expected: { x: -0.145267742, y: -0.989392381 } },
+    { gradient: new RandomGradient(new Point(101, 201)), expected: { x: 0.263549536, y: -0.964645864 } }
   ]
 
   for (const randomGradient of randomGradients) {
