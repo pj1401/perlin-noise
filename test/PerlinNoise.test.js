4b356e6b7f26c01abca5d4136c2c2174d2ad0e4f
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-08 17:42:27 +0200

    Add tests for interpolation method

--- test/PerlinNoise.test.js
+++ test/PerlinNoise.test.js
@@ -77,3 +77,47 @@ test('Fade', () => {
     expect(perlin.fade(fadeValue.difference)).toBeCloseTo(fadeValue.expected)
   }
 })
+
+test('Interpolation', () => {
+  const perlin = new PerlinNoise(1.3, 2.1)
+
+  const testValues = [
+    {
+      a: 0.129869565800774,
+      b: -0.105517730672436,
+      fade: 0.16308,
+      expected: 0.091482605491923
+    }, {
+      a: 0.595001439253961,
+      b: -0.87287104641861,
+      fade: 0.16308,
+      expected: 0.355620794290478
+    }, {
+      a: 0.84860770827331,
+      b: -0.830807742805546,
+      fade: 0.5,
+      expected: 0.008899982733882
+    }, {
+      a: 0,
+      b: -0.762921343630262,
+      fade: 0.5,
+      expected: -0.381460671815131
+    },
+    {
+      a: 0.0088999827338817,
+      b: 0,
+      fade: 0.83692,
+      expected: 0.001451409184241
+    }, {
+      a: 0.124762150584332,
+      b: 0.701096327945958,
+      fade: 0,
+      expected: 0.124762150584332
+    }, { a: 0.5, b: -0.5, fade: 0.5, expected: 0 },
+    { a: 0.5, b: 0.5, fade: 0.5, expected: 0.5 }
+  ]
+
+  for (const value of testValues) {
+    expect(perlin.interpolate(value.a, value.b, value.fade)).toBeCloseTo(value.expected)
+  }
+})
