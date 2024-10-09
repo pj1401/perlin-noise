7a541fc1363ab155d64dbe8541236892a1d42a58
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-09 15:39:28 +0200

    Delete Gradient.js

--- test/PerlinNoise.test.js
+++ test/PerlinNoise.test.js
@@ -5,7 +5,6 @@
  * @version 1.0.0
  */
 
-import { Gradient } from '../src/Gradient.js'
 import { PerlinNoise } from '../src/PerlinNoise.js'
 import { Vector } from '../src/Vector.js'
 
@@ -27,31 +26,31 @@ test('Dot products', () => {
   const perlin = new PerlinNoise(1.3, 2.1)
 
   const dotProducts = [{
-    gradient: new Gradient(0.101279205538, 0.994858041394),
+    gradient: new Vector(0.101279205538, 0.994858041394),
     vector: new Vector(0.3, 0.1),
     expected: 0.1298695658008
   }, {
-    gradient: new Gradient(0.007886915561, -0.999968897798),
+    gradient: new Vector(0.007886915561, -0.999968897798),
     vector: new Vector(-0.7, 0.1),
     expected: -0.1055177306725
   }, {
-    gradient: new Gradient(0.937233865382, -0.348701421821),
+    gradient: new Vector(0.937233865382, -0.348701421821),
     vector: new Vector(-0.3, -0.9),
     expected: 0.0326611200243
   }, {
-    gradient: new Gradient(-0.037835073065, 0.999283997293),
+    gradient: new Vector(-0.037835073065, 0.999283997293),
     vector: new Vector(-0.7, -0.9),
     expected: -0.8728710464182
   }, {
-    gradient: new Gradient(-0.992802137105, 0.119766091022),
+    gradient: new Vector(-0.992802137105, 0.119766091022),
     vector: new Vector(0, 0),
     expected: 0
   }, {
-    gradient: new Gradient(0.263549535693, -0.964645863639),
+    gradient: new Vector(0.263549535693, -0.964645863639),
     vector: new Vector(-1, -1),
     expected: 0.701096327946
   }, {
-    gradient: new Gradient(1, 0),
+    gradient: new Vector(1, 0),
     vector: new Vector(0.5, 0.5),
     expected: 0.5
   }
