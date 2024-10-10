be735dd4fd59174294142a30bf4655b8591e1b2c
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-10 17:14:31 +0200

    Remove dot product test from PerlinNoise.test.js

--- test/PerlinNoise.test.js
+++ test/PerlinNoise.test.js
@@ -6,7 +6,6 @@
  */
 
 import { PerlinNoise } from '../src/PerlinNoise.js'
-// import { Vector } from '../src/Vector.js'
 
 test('Perlin noise values', () => {
   const perlinNoises = [
@@ -21,47 +20,6 @@ test('Perlin noise values', () => {
     expect(perlinNoise.perlin.valueOf()).toBeCloseTo(perlinNoise.expected)
   }
 })
-
-/*
-test('Dot products', () => {
-  const perlin = new PerlinNoise(1.3, 2.1)
-
-  const dotProducts = [{
-    gradient: new Vector(0.101279205538, 0.994858041394),
-    vector: new Vector(0.3, 0.1),
-    expected: 0.1298695658008
-  }, {
-    gradient: new Vector(0.007886915561, -0.999968897798),
-    vector: new Vector(-0.7, 0.1),
-    expected: -0.1055177306725
-  }, {
-    gradient: new Vector(0.937233865382, -0.348701421821),
-    vector: new Vector(-0.3, -0.9),
-    expected: 0.0326611200243
-  }, {
-    gradient: new Vector(-0.037835073065, 0.999283997293),
-    vector: new Vector(-0.7, -0.9),
-    expected: -0.8728710464182
-  }, {
-    gradient: new Vector(-0.992802137105, 0.119766091022),
-    vector: new Vector(0, 0),
-    expected: 0
-  }, {
-    gradient: new Vector(0.263549535693, -0.964645863639),
-    vector: new Vector(-1, -1),
-    expected: 0.701096327946
-  }, {
-    gradient: new Vector(1, 0),
-    vector: new Vector(0.5, 0.5),
-    expected: 0.5
-  }
-  ]
-
-  for (const dotPorduct of dotProducts) {
-    expect(perlin.dotProduct(dotPorduct.gradient, dotPorduct.vector)).toBeCloseTo(dotPorduct.expected)
-  }
-})
-*/
 
 /*
 test('Fade', () => {
