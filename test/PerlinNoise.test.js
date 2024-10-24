263469bd0ff87b91a71476225c932fa8ac190358
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-24 20:36:35 +0200

    Refactor the perlin noise tests

--- test/PerlinNoise.test.js
+++ test/PerlinNoise.test.js
@@ -7,81 +7,116 @@
 
 import PerlinNoise from '../index.js'
 
-test('Perlin noise values', () => {
-  const perlin = new PerlinNoise()
-  const perlinNoises = [
-    { generatedValue: perlin.generatePerlinNoise(1.3, 2.1), expected: 0.093744 },
-    { generatedValue: perlin.generatePerlinNoise(5.5, 3.7), expected: -0.177968 },
-    { generatedValue: perlin.generatePerlinNoise(10.0, 20.0), expected: 0 },
-    { generatedValue: perlin.generatePerlinNoise(100.0, 200.0), expected: 0 },
-    { generatedValue: perlin.generatePerlinNoise(0, 0.5), expected: -0.056895 },
-    { generatedValue: perlin.generatePerlinNoise(0.1, 0.5), expected: -0.05546 },
-    { generatedValue: perlin.generatePerlinNoise(0.2, 0.5), expected: -0.051994 },
-    { generatedValue: perlin.generatePerlinNoise(0.3, 0.5), expected: -0.042475 },
-    { generatedValue: perlin.generatePerlinNoise(0.4, 0.5), expected: -0.023314 },
-    { generatedValue: perlin.generatePerlinNoise(0.5, 0.5), expected: 0.006776 },
-    { generatedValue: perlin.generatePerlinNoise(0.5, 0.6), expected: -0.00591 },
-    { generatedValue: perlin.generatePerlinNoise(0.5, 0.7), expected: -0.028887 },
-    { generatedValue: perlin.generatePerlinNoise(0.5, 0.8), expected: -0.059371 },
-    { generatedValue: perlin.generatePerlinNoise(0.5, 0.9), expected: -0.092953 },
-    { generatedValue: perlin.generatePerlinNoise(0.5, 1), expected: -0.126147 }
-  ]
-
-  for (const perlinNoise of perlinNoises) {
-    expect(perlinNoise.generatedValue).toBeCloseTo(perlinNoise.expected)
+/**
+ * Represents a test case for perlin noise values.
+ */
+class PerlinNoiseTestCase {
+  /**
+   * Initialises the object.
+   *
+   * @param {number} x - The x-coordinate.
+   * @param {number} y - The y-coordinate.
+   * @param {number} expected - The expected noise value.
+   */
+  constructor (x, y, expected) {
+    this.x = x
+    this.y = y
+    this.expected = expected
   }
+
+  /**
+   * Run the test case.
+   *
+   * @param {PerlinNoise} perlin - The PerlinNoise object.
+   */
+  runTest (perlin) {
+    const actual = perlin.generatePerlinNoise(this.x, this.y)
+    expect(actual).toBeCloseTo(this.expected)
+  }
+}
+
+describe('Perlin noise values', () => {
+  test('Perlin noise with undefined seed', () => {
+    const perlin = new PerlinNoise()
+    const testCases = [
+      new PerlinNoiseTestCase(1.3, 2.1, 0.093744),
+      new PerlinNoiseTestCase(5.5, 3.7, -0.177968),
+      new PerlinNoiseTestCase(10.0, 20.0, 0),
+      new PerlinNoiseTestCase(100.0, 200.0, 0),
+      new PerlinNoiseTestCase(0, 0.5, -0.056895),
+      new PerlinNoiseTestCase(0.1, 0.5, -0.05546),
+      new PerlinNoiseTestCase(0.2, 0.5, -0.051994),
+      new PerlinNoiseTestCase(0.3, 0.5, -0.042475),
+      new PerlinNoiseTestCase(0.4, 0.5, -0.023314),
+      new PerlinNoiseTestCase(0.5, 0.5, 0.006776),
+      new PerlinNoiseTestCase(0.5, 0.6, -0.00591),
+      new PerlinNoiseTestCase(0.5, 0.7, -0.028887),
+      new PerlinNoiseTestCase(0.5, 0.8, -0.059371),
+      new PerlinNoiseTestCase(0.5, 0.9, -0.092953),
+      new PerlinNoiseTestCase(0.5, 1, -0.126147)
+    ]
+
+    for (const testCase of testCases) {
+      testCase.runTest(perlin)
+    }
+  })
+
+  test('Using a seed', () => {
+    // TODO: Should there be a different class for these test cases?
+    const perlin = new PerlinNoise(4783)
+    const testCases = [
+      { perlin: new PerlinNoise(1.3, 2.1, 4783), expected: 0.028378 },
+      { perlin: new PerlinNoise(5.5, 3.7, 4783), expected: -0.223627 },
+      { perlin: new PerlinNoise(10, 20, 4783), expected: 0 },
+      { perlin: new PerlinNoise(1.3, 2.1, 6829), expected: 0.206701 },
+      { perlin: new PerlinNoise(5.5, 3.7, 6829), expected: 0.169030 },
+      { perlin: new PerlinNoise(10, 20, 6829), expected: 0 },
+      { perlin: new PerlinNoise(1.3, 2.1, 7001), expected: -0.166117 },
+      { perlin: new PerlinNoise(5.5, 3.7, 7001), expected: 0.183346 },
+      { perlin: new PerlinNoise(10, 20, 7001), expected: 0 },
+      { perlin: new PerlinNoise(0, 0.5, 24815821), expected: -0.3184 },
+      { perlin: new PerlinNoise(0.1, 0.5, 24815821), expected: -0.391194 },
+      { perlin: new PerlinNoise(0.2, 0.5, 24815821), expected: -0.442847 },
+      { perlin: new PerlinNoise(0.3, 0.5, 24815821), expected: -0.45581 },
+      { perlin: new PerlinNoise(0.4, 0.5, 24815821), expected: -0.421792 },
+      { perlin: new PerlinNoise(0.5, 0.5, 24815821), expected: -0.344707 },
+      { perlin: new PerlinNoise(0.5, 0.6, 24815821), expected: -0.350501 },
+      { perlin: new PerlinNoise(0.5, 0.7, 24815821), expected: -0.325892 },
+      { perlin: new PerlinNoise(0.5, 0.8, 24815821), expected: -0.272131 },
+      { perlin: new PerlinNoise(0.5, 0.9, 24815821), expected: -0.197691 },
+      { perlin: new PerlinNoise(0.5, 1, 24815821), expected: -0.114196 }
+    ]
+
+    for (const testCase of testCases) {
+      testCase.runTest(perlin)
+    }
+  })
 })
 
 test('Changing coordinates', () => {
-  const perlin = new PerlinNoise(1.3, 2.1)
+  const perlin = new PerlinNoise()
+  perlin.generatePerlinNoise(1.3, 2.1)
   expect(perlin.valueOf()).toBeCloseTo(0.093744)
 
   perlin.generatePerlinNoise(3.5, 2.1)
 
-  expect(perlin.valueOf()).toBeCloseTo(0.22739)
-})
-
-test('Using a seed', () => {
-  const perlinNoises = [
-    { perlin: new PerlinNoise(1.3, 2.1, 4783), expected: 0.028378 },
-    { perlin: new PerlinNoise(5.5, 3.7, 4783), expected: -0.223627 },
-    { perlin: new PerlinNoise(10, 20, 4783), expected: 0 },
-    { perlin: new PerlinNoise(1.3, 2.1, 6829), expected: 0.206701 },
-    { perlin: new PerlinNoise(5.5, 3.7, 6829), expected: 0.169030 },
-    { perlin: new PerlinNoise(10, 20, 6829), expected: 0 },
-    { perlin: new PerlinNoise(1.3, 2.1, 7001), expected: -0.166117 },
-    { perlin: new PerlinNoise(5.5, 3.7, 7001), expected: 0.183346 },
-    { perlin: new PerlinNoise(10, 20, 7001), expected: 0 },
-    { perlin: new PerlinNoise(0, 0.5, 24815821), expected: -0.3184 },
-    { perlin: new PerlinNoise(0.1, 0.5, 24815821), expected: -0.391194 },
-    { perlin: new PerlinNoise(0.2, 0.5, 24815821), expected: -0.442847 },
-    { perlin: new PerlinNoise(0.3, 0.5, 24815821), expected: -0.45581 },
-    { perlin: new PerlinNoise(0.4, 0.5, 24815821), expected: -0.421792 },
-    { perlin: new PerlinNoise(0.5, 0.5, 24815821), expected: -0.344707 },
-    { perlin: new PerlinNoise(0.5, 0.6, 24815821), expected: -0.350501 },
-    { perlin: new PerlinNoise(0.5, 0.7, 24815821), expected: -0.325892 },
-    { perlin: new PerlinNoise(0.5, 0.8, 24815821), expected: -0.272131 },
-    { perlin: new PerlinNoise(0.5, 0.9, 24815821), expected: -0.197691 },
-    { perlin: new PerlinNoise(0.5, 1, 24815821), expected: -0.114196 }
-  ]
-
-  for (const perlinNoise of perlinNoises) {
-    expect(perlinNoise.perlin.valueOf()).toBeCloseTo(perlinNoise.expected)
-  }
+  expect(perlin.generatePerlinNoise(3.5, 2.1)).toBeCloseTo(0.22739)
 })
 
 test('NaN in coordinates', () => {
-  expect(() => new PerlinNoise(NaN, 2)).toThrow(TypeError)
+  const perlin = new PerlinNoise()
+  expect(() => perlin.generatePerlinNoise(NaN, 2)).toThrow(TypeError)
 })
 
 test('Non number in coordinates', () => {
-  expect(() => new PerlinNoise(1, 'number')).toThrow(TypeError)
-  expect(() => new PerlinNoise('4', 3)).toThrow(TypeError)
+  const perlin = new PerlinNoise()
+  expect(() => perlin.generatePerlinNoise(1, 'number')).toThrow(TypeError)
+  expect(() => perlin.generatePerlinNoise('4', 3)).toThrow(TypeError)
 })
 
 test('Changing coordinates to non number', () => {
-  const perlin = new PerlinNoise(1.3, 2.1)
-  expect(perlin.valueOf()).toBeCloseTo(0.093744)
+  const perlin = new PerlinNoise()
+  expect(perlin.generatePerlinNoise(1.3, 2.1)).toBeCloseTo(0.093744)
 
   expect(() => { perlin.generatePerlinNoise('1.3', 2.1) }).toThrow(TypeError)
 
@@ -89,9 +124,9 @@ test('Changing coordinates to non number', () => {
 })
 
 test('Setting seed to non number', () => {
-  expect(() => new PerlinNoise(1.3, 2.1, '4783')).toThrow(TypeError)
-  expect(() => new PerlinNoise(1.3, 2.1, 'number')).toThrow(TypeError)
-  expect(() => new PerlinNoise(1.3, 2.1, NaN)).toThrow(TypeError)
+  expect(() => new PerlinNoise('4783')).toThrow(TypeError)
+  expect(() => new PerlinNoise('number')).toThrow(TypeError)
+  expect(() => new PerlinNoise(NaN)).toThrow(TypeError)
 })
 
 /*
