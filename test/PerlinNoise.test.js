6a7247029e52d6de330eaa8b478a2064a10451ca
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-25 09:34:49 +0200

    Refactor PerlinNoise tests

--- test/PerlinNoise.test.js
+++ test/PerlinNoise.test.js
@@ -62,76 +62,86 @@ describe('Perlin noise values', () => {
   })
 
   test('Using a seed', () => {
-    // TODO: Should there be a different class for these test cases?
-    const perlin = new PerlinNoise(4783)
-    const testCases = [
-      { perlin: new PerlinNoise(1.3, 2.1, 4783), expected: 0.028378 },
-      { perlin: new PerlinNoise(5.5, 3.7, 4783), expected: -0.223627 },
-      { perlin: new PerlinNoise(10, 20, 4783), expected: 0 },
-      { perlin: new PerlinNoise(1.3, 2.1, 6829), expected: 0.206701 },
-      { perlin: new PerlinNoise(5.5, 3.7, 6829), expected: 0.169030 },
-      { perlin: new PerlinNoise(10, 20, 6829), expected: 0 },
-      { perlin: new PerlinNoise(1.3, 2.1, 7001), expected: -0.166117 },
-      { perlin: new PerlinNoise(5.5, 3.7, 7001), expected: 0.183346 },
-      { perlin: new PerlinNoise(10, 20, 7001), expected: 0 },
-      { perlin: new PerlinNoise(0, 0.5, 24815821), expected: -0.3184 },
-      { perlin: new PerlinNoise(0.1, 0.5, 24815821), expected: -0.391194 },
-      { perlin: new PerlinNoise(0.2, 0.5, 24815821), expected: -0.442847 },
-      { perlin: new PerlinNoise(0.3, 0.5, 24815821), expected: -0.45581 },
-      { perlin: new PerlinNoise(0.4, 0.5, 24815821), expected: -0.421792 },
-      { perlin: new PerlinNoise(0.5, 0.5, 24815821), expected: -0.344707 },
-      { perlin: new PerlinNoise(0.5, 0.6, 24815821), expected: -0.350501 },
-      { perlin: new PerlinNoise(0.5, 0.7, 24815821), expected: -0.325892 },
-      { perlin: new PerlinNoise(0.5, 0.8, 24815821), expected: -0.272131 },
-      { perlin: new PerlinNoise(0.5, 0.9, 24815821), expected: -0.197691 },
-      { perlin: new PerlinNoise(0.5, 1, 24815821), expected: -0.114196 }
+    const perlinGenerators = [
+      new PerlinNoise(4783),
+      new PerlinNoise(6829),
+      new PerlinNoise(7001),
+      new PerlinNoise(24815821)
     ]
 
-    for (const testCase of testCases) {
+    const testCases = [
+      { perlin: perlinGenerators[0], testCase: new PerlinNoiseTestCase(1.3, 2.1, 0.028378) },
+      { perlin: perlinGenerators[0], testCase: new PerlinNoiseTestCase(5.5, 3.7, -0.223627) },
+      { perlin: perlinGenerators[0], testCase: new PerlinNoiseTestCase(10, 20, 0) },
+      { perlin: perlinGenerators[1], testCase: new PerlinNoiseTestCase(1.3, 2.1, 0.206701) },
+      { perlin: perlinGenerators[1], testCase: new PerlinNoiseTestCase(5.5, 3.7, 0.169030) },
+      { perlin: perlinGenerators[1], testCase: new PerlinNoiseTestCase(10, 20, 0) },
+      { perlin: perlinGenerators[2], testCase: new PerlinNoiseTestCase(1.3, 2.1, -0.166117) },
+      { perlin: perlinGenerators[2], testCase: new PerlinNoiseTestCase(5.5, 3.7, 0.183346) },
+      { perlin: perlinGenerators[2], testCase: new PerlinNoiseTestCase(10, 20, 0) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0, 0.5, -0.3184) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0.1, 0.5, -0.391194) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0.2, 0.5, -0.442847) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0.3, 0.5, -0.45581) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0.4, 0.5, -0.421792) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0.5, 0.5, -0.344707) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0.5, 0.6, -0.350501) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0.5, 0.7, -0.325892) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0.5, 0.8, -0.272131) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0.5, 0.9, -0.197691) },
+      { perlin: perlinGenerators[3], testCase: new PerlinNoiseTestCase(0.5, 1, -0.114196) }
+    ]
+
+    for (const { perlin, testCase } of testCases) {
       testCase.runTest(perlin)
     }
   })
+
+  test('Using valueOf()', () => {
+    const perlin = new PerlinNoise()
+    perlin.generatePerlinNoise(1.3, 2.1)
+    expect(perlin.valueOf()).toBeCloseTo(0.093744)
+
+    perlin.generatePerlinNoise(3.5, 2.1)
+
+    expect(perlin.valueOf()).toBeCloseTo(0.22739)
+  })
 })
 
-test('Changing coordinates', () => {
-  const perlin = new PerlinNoise()
-  perlin.generatePerlinNoise(1.3, 2.1)
-  expect(perlin.valueOf()).toBeCloseTo(0.093744)
+describe('exceptions', () => {
+  test('NaN in coordinates', () => {
+    const perlin = new PerlinNoise()
+    expect(() => perlin.generatePerlinNoise(NaN, 2)).toThrow(TypeError)
+  })
 
-  perlin.generatePerlinNoise(3.5, 2.1)
+  test('Non number in coordinates', () => {
+    const perlin = new PerlinNoise()
+    expect(() => perlin.generatePerlinNoise(1, 'number')).toThrow(TypeError)
+    expect(() => perlin.generatePerlinNoise('4', 3)).toThrow(TypeError)
+  })
 
-  expect(perlin.generatePerlinNoise(3.5, 2.1)).toBeCloseTo(0.22739)
+  test('Value remains unchanged', () => {
+    const perlin = new PerlinNoise()
+    expect(perlin.generatePerlinNoise(1.3, 2.1)).toBeCloseTo(0.093744)
+
+    expect(() => { perlin.generatePerlinNoise('1.3', 2.1) }).toThrow(TypeError)
+
+    expect(perlin.valueOf()).toBeCloseTo(0.093744)
+  })
+
+  test('Setting seed to non number', () => {
+    expect(() => new PerlinNoise('4783')).toThrow(TypeError)
+    expect(() => new PerlinNoise('number')).toThrow(TypeError)
+    expect(() => new PerlinNoise(NaN)).toThrow(TypeError)
+  })
 })
 
-test('NaN in coordinates', () => {
-  const perlin = new PerlinNoise()
-  expect(() => perlin.generatePerlinNoise(NaN, 2)).toThrow(TypeError)
-})
-
-test('Non number in coordinates', () => {
-  const perlin = new PerlinNoise()
-  expect(() => perlin.generatePerlinNoise(1, 'number')).toThrow(TypeError)
-  expect(() => perlin.generatePerlinNoise('4', 3)).toThrow(TypeError)
-})
-
-test('Changing coordinates to non number', () => {
-  const perlin = new PerlinNoise()
-  expect(perlin.generatePerlinNoise(1.3, 2.1)).toBeCloseTo(0.093744)
-
-  expect(() => { perlin.generatePerlinNoise('1.3', 2.1) }).toThrow(TypeError)
-
-  expect(perlin.valueOf()).toBeCloseTo(0.093744)
-})
-
-test('Setting seed to non number', () => {
-  expect(() => new PerlinNoise('4783')).toThrow(TypeError)
-  expect(() => new PerlinNoise('number')).toThrow(TypeError)
-  expect(() => new PerlinNoise(NaN)).toThrow(TypeError)
-})
+// These tests are only used for debugging.
+// The tested method has to be made public for the test to run.
 
 /*
 test('Fade', () => {
-  const perlin = new PerlinNoise(1.3, 2.1)
+  const perlin = new PerlinNoise()
 
   const fadeValues = [
     { difference: 0, expected: 0 },
@@ -149,7 +159,7 @@ test('Fade', () => {
 
 /*
 test('Interpolation', () => {
-  const perlin = new PerlinNoise(1.3, 2.1)
+  const perlin = new PerlinNoise()
 
   const testValues = [
     {
